3708d240f2b09660a9e26cceda5be54b56ddae8c
Merlin <acewzk@gmail.com>
2023-09-05 10:53:39 +0800

    refactor: clean dep

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
-require("hardhat-gas-reporter");
-require('solidity-coverage')
+require("hardhat-contract-sizer")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: {
-        version: "0.8.19",
+  solidity: "0.8.19",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
     },
-    defaultNetwork: "hardhat",
-    networks: {
-        goerli: {
-            url: process.env.GOERLI_RPC,
-            accounts: [process.env.PRIVATEKEY]
-        },
-        sepolia: {
-            url: process.env.SEPOLIA_RPC,
-            accounts: [process.env.PRIVATEKEY]
-        },
-        mumbai: {
-            url: process.env.MUMBAI_RPC,
-            gasPrice: 35000000000,
-            accounts: [process.env.PRIVATEKEY]
-        },
-        hardhat: {
-            allowUnlimitedContractSize: true
-        },
-        dev: {
-            url: process.env.DEV_RPC,
-            allowUnlimitedContractSize: true,
-            accounts: [process.env.PRIVATEKEY]
-        },
-    },
-    etherscan: {
-        apiKey: process.env.POLYGONSCAN_API_KEY
-    },
-}
+    viaIR: true,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+};
