18321e396c62744a11e5ca80dbb53d7aee773a2b
Merlin <acewzk@gmail.com>
2024-01-31 19:16:45 +0800

    feat: ccipReceive sender check

--- contracts/ccip/RateReceiver.sol
+++ contracts/ccip/RateReceiver.sol
@@ -7,6 +7,8 @@ import "./interface/ICCIPRateProvider.sol";
 import "./Types.sol";
 
 contract RateReceiver is CCIPReceiver {
+    error TransferNotAllow();
+
     // Event emitted when a message is received from another chain.
     event MessageReceived(
         bytes32 indexed messageId, // The unique ID of the message.
@@ -19,19 +21,27 @@ contract RateReceiver is CCIPReceiver {
     bytes32 private lastReceivedMessageId; // Store the last received messageId.
     bytes private lastReceivedData; // Store the last received bytes data.
 
-    constructor(address _router) CCIPReceiver(_router) {}
+    address private allowSender;
+
+    constructor(address _router, address _allowSender) CCIPReceiver(_router) {
+        allowSender = _allowSender;
+    }
+
+    modifier onlyAllowSender() {
+        if (allowSender != msg.sender) {
+            revert TransferNotAllow();
+        }
+        _;
+    }
 
     /// handle a received message
     function _ccipReceive(
         Client.Any2EVMMessage memory any2EvmMessage
-    ) internal override {
+    ) internal override onlyAllowSender {
         lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
         lastReceivedData = any2EvmMessage.data;
 
-        RateMsg memory rateMsg = abi.decode(
-            lastReceivedData,
-            (RateMsg)
-        );
+        RateMsg memory rateMsg = abi.decode(lastReceivedData, (RateMsg));
         ICCIPRateProvider(rateMsg.destination).setRate(rateMsg.rate);
 
         emit MessageReceived(
