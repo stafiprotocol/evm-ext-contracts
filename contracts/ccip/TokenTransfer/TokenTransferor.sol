a7df6615856f1936c1cbea6e886417d292c6d08c
Merlin <acewzk@gmail.com>
2024-07-30 18:39:21 +0800

    feat: token transfer rm pay for link

--- contracts/ccip/TokenTransfer/TokenTransferor.sol
+++ contracts/ccip/TokenTransfer/TokenTransferor.sol
@@ -20,7 +20,6 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     IRouterClient private s_router;
-    IERC20 private s_linkToken;
 
     mapping(uint64 => bool) public allowlistedChains;
     mapping(address => bool) public allowlistedTokens;
@@ -47,7 +46,6 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
     error DestinationChainNotAllowlisted(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
     error InvalidReceiverAddress(); // Used when the receiver address is 0.
     error InsufficientAllowance(uint256 currentAllowance, uint256 requiredAmount);
-    error InsufficientLINKBalance(uint256 currentBalance, uint256 requiredAmount);
     error TokenNotAllowlisted(address token);
     error InvalidAddress(string param);
     error InvalidContract(string param);
@@ -85,16 +83,13 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
     /// @notice Initialize the contract
     /// @dev This function replaces the constructor for upgradeable contracts
     /// @param _router Address of the CCIP router
-    /// @param _link Address of the LINK token
     /// @param _admin Address of the initial admin
-    function initialize(address _router, address _link, address _admin) public initializer {
+    function initialize(address _router, address _admin) public initializer {
         if (_router == address(0)) revert InvalidAddress("router");
-        if (_link == address(0)) revert InvalidAddress("link");
         if (_admin == address(0)) revert InvalidAddress("admin");
 
         // Basic contract existence check
         if (!_isContract(_router)) revert InvalidContract("router");
-        if (!_isContract(_link)) revert InvalidContract("link");
 
         __AccessControl_init();
         __Pausable_init();
@@ -105,7 +100,6 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
         _grantRole(PAUSER_ROLE, _admin);
 
         s_router = IRouterClient(_router);
-        s_linkToken = IERC20(_link);
     }
 
     /// @notice Pause the contract
@@ -138,61 +132,6 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
         emit TokenAllowlistUpdated(_token, _allowed);
     }
 
-    function transferTokens(
-        uint64 _destinationChainSelector,
-        address _receiver,
-        address _token,
-        uint256 _amount,
-        address _feeToken
-    )
-        internal
-        nonReentrant
-        validateReceiver(_receiver)
-        onlyAllowlistedToken(_token)
-        onlyAllowlistedChain(_destinationChainSelector)
-        returns (bytes32 messageId)
-    {
-        // Check if the contract has enough allowance to spend user's tokens
-        uint256 currentAllowance = IERC20(_token).allowance(msg.sender, address(this));
-        if (currentAllowance < _amount) {
-            revert InsufficientAllowance(currentAllowance, _amount);
-        }
-
-        // Create an EVM2AnyMessage struct in memory
-        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(_receiver, _token, _amount, _feeToken);
-
-        // Get the fee required to send the message
-        uint256 fees = s_router.getFee(_destinationChainSelector, evm2AnyMessage);
-
-        // Check if the contract has enough balance to pay for the fees
-        if (_feeToken == address(0)) {
-            if (fees > address(this).balance) {
-                revert NotEnoughBalance(address(this).balance, fees);
-            }
-        } else {
-            uint256 linkBalance = IERC20(_feeToken).balanceOf(address(this));
-            if (linkBalance < fees) {
-                revert InsufficientLINKBalance(linkBalance, fees);
-            }
-            IERC20(_feeToken).safeIncreaseAllowance(address(s_router), fees);
-        }
-
-        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-
-        // Approve the Router to spend tokens on contract's behalf
-        IERC20(_token).safeIncreaseAllowance(address(s_router), _amount);
-
-        // Send the message through the router and store the returned message ID
-        messageId = _feeToken == address(0)
-            ? s_router.ccipSend{value: fees}(_destinationChainSelector, evm2AnyMessage)
-            : s_router.ccipSend(_destinationChainSelector, evm2AnyMessage);
-
-        // Emit an event with message details
-        emit TokensTransferred(messageId, _destinationChainSelector, _receiver, _token, _amount, _feeToken, fees);
-
-        return messageId;
-    }
-
     /// @notice Transfer tokens to receiver on the destination chain.
     /// @notice Pay in native gas such as ETH on Ethereum or MATIC on Polgon.
     /// @notice the token must be in the list of supported tokens.
@@ -202,31 +141,53 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
     /// @param _token token address.
     /// @param _amount token amount.
     /// @return messageId The ID of the message that was sent.
-    function transferTokensPayNative(
+    function transferTokens(
         uint64 _destinationChainSelector,
         address _receiver,
         address _token,
-        uint256 _amount
-    ) external nonReentrant returns (bytes32 messageId) {
-        return transferTokens(_destinationChainSelector, _receiver, _token, _amount, address(0));
-    }
+        uint256 _amount,
+        address _feeToken
+    )
+    external
+    nonReentrant
+    validateReceiver(_receiver)
+    onlyAllowlistedToken(_token)
+    onlyAllowlistedChain(_destinationChainSelector)
+    returns (bytes32 messageId)
+    {
+        // Check if the contract has enough allowance to spend user's tokens
+        uint256 currentAllowance = IERC20(_token).allowance(msg.sender, address(this));
+        if (currentAllowance < _amount) {
+            revert InsufficientAllowance(currentAllowance, _amount);
+        }
 
-    /// @notice Transfer tokens to receiver on the destination chain.
-    /// @notice pay in LINK.
-    /// @notice the token must be in the list of supported tokens.
-    /// @dev Assumes your contract has sufficient LINK tokens to pay for the fees.
-    /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
-    /// @param _receiver The address of the recipient on the destination blockchain.
-    /// @param _token token address.
-    /// @param _amount token amount.
-    /// @return messageId The ID of the message that was sent.
-    function transferTokensPayLINK(
-        uint64 _destinationChainSelector,
-        address _receiver,
-        address _token,
-        uint256 _amount
-    ) external nonReentrant returns (bytes32 messageId) {
-        return transferTokens(_destinationChainSelector, _receiver, _token, _amount, address(s_linkToken));
+        // Create an EVM2AnyMessage struct in memory
+        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(_receiver, _token, _amount, address(0));
+
+        // Get the fee required to send the message
+        uint256 fees = s_router.getFee(_destinationChainSelector, evm2AnyMessage);
+
+        // Check if the contract has enough balance to pay for the fees
+        if (fees > address(this).balance) {
+            revert NotEnoughBalance(address(this).balance, fees);
+        }
+
+
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // Approve the Router to spend tokens on contract's behalf
+        IERC20(_token).safeIncreaseAllowance(address(s_router), _amount);
+
+        // Send the message through the router and store the returned message ID
+        messageId = s_router.ccipSend{value: fees}(
+            _destinationChainSelector,
+            evm2AnyMessage
+        );
+
+        // Emit an event with message details
+        emit TokensTransferred(messageId, _destinationChainSelector, _receiver, _token, _amount, _feeToken, fees);
+
+        return messageId;
     }
 
     /// @notice Build a CCIP message
@@ -247,44 +208,12 @@ contract TokenTransferor is Initializable, AccessControlUpgradeable, PausableUpg
 
         return
             Client.EVM2AnyMessage({
-                receiver: abi.encode(_receiver),
-                data: "",
-                tokenAmounts: tokenAmounts,
-                extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 0})),
-                feeToken: _feeTokenAddress
-            });
-    }
-
-    /// @notice Allows the contract admin to withdraw the entire balance of Ether from the contract.
-    /// @dev This function reverts if there are no funds to withdraw or if the transfer fails.
-    /// It should only be callable by the admin of the contract.
-    /// @param _beneficiary The address to which the Ether should be transferred.
-    function withdraw(address _beneficiary) public onlyRole(ADMIN_ROLE) {
-        // Retrieve the balance of this contract
-        uint256 amount = address(this).balance;
-
-        // Revert if there is nothing to withdraw
-        if (amount == 0) revert NothingToWithdraw();
-
-        // Attempt to send the funds, capturing the success status and discarding any return data
-        (bool sent, ) = _beneficiary.call{value: amount}("");
-
-        // Revert if the send failed, with information about the attempted transfer
-        if (!sent) revert FailedToWithdrawEth(msg.sender, _beneficiary, amount);
-    }
-
-    /// @notice Allows the admin of the contract to withdraw all tokens of a specific ERC20 token.
-    /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
-    /// @param _beneficiary The address to which the tokens will be sent.
-    /// @param _token The contract address of the ERC20 token to be withdrawn.
-    function withdrawToken(address _beneficiary, address _token) public onlyRole(ADMIN_ROLE) {
-        // Retrieve the balance of this contract
-        uint256 amount = IERC20(_token).balanceOf(address(this));
-
-        // Revert if there is nothing to withdraw
-        if (amount == 0) revert NothingToWithdraw();
-
-        IERC20(_token).safeTransfer(_beneficiary, amount);
+            receiver: abi.encode(_receiver),
+            data: "",
+            tokenAmounts: tokenAmounts,
+            extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 0})),
+            feeToken: _feeTokenAddress
+        });
     }
 
     // Helper function to check if an address is a contract
